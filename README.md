7556e1641e1daf3d824273399a4cf8dc8ce6314d
viktar hameza <viktor.hameza@gmail.com>
2019-07-18 11:15:17 +0300

    registration routes

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# clock-r
+# courses-store (NodeJS, Express, Handlebars)
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const path = require("path");
 const exphbs = require("express-handlebars");
+const homeRoutes = require('./routes/home');
+const coursesRoutes = require('./routes/courses');
+const addRoutes = require('./routes/add');
+
 
 const app = express();
 const hbs = exphbs.create({
@@ -13,25 +17,9 @@ app.set("view engine", "hbs");
 app.set("views", "views");
 
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.render("index", {
-    title: "Home",
-    isHome: true
-  });
-});
-app.get("/courses", (req, res) => {
-  res.render("courses", {
-    title: "Course",
-    isCourses: true
-  });
-});
-app.get("/add", (req, res) => {
-  res.render("add", {
-    title: "Add course",
-    isAdd: true
-  });
-});
+app.use("/", homeRoutes);
+app.use("/courses", coursesRoutes);
+app.use("/add", addRoutes);
 
 
 const PORT = process.env.PORT || 2000;
